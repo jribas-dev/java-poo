bffbe3fa5d70566cd4c048eed6657d187ee8d0e3
jribas-dev <3b3brazil@gmail.com>
2023-03-01 01:55:58 -0300

    Exercicio desvendar crime.

---
+++ src/br/com/curso/collection/ExCenaCrime.java
@@ -0,0 +1,50 @@
+package br.com.curso.collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class ExCenaCrime {
+
+	public static void main(String[] args) {
+		List<String> perguntas = new ArrayList<>(
+				Arrays.asList(
+						"Telefonou para a vítima?", 
+						"Esteve no local do crime?",
+						"Mora perto da vítima?", 
+						"Devia para a vítima?", 
+						"Já trabalhou com a vítima?")
+				);
+		Scanner scan = new Scanner(System.in);
+		int respostas = 0;
+
+		for (int i = 0; i < perguntas.size(); i++) {
+			System.out.println((i + 1) + " - " + perguntas.get(i) + " S/N");
+			if (scan.next().compareToIgnoreCase("S") == 0)
+				respostas++;
+		}
+
+		desvendarCrime(respostas);
+		scan.close();
+	}
+
+	private static void desvendarCrime(int r) {
+		switch (r) {
+		case 1:
+			System.out.println("Inocente");
+			break;
+		case 2:
+			System.out.println("Suspeita");
+			break;
+		case 3:
+		case 4:
+			System.out.println("Cúmplice");
+			break;
+		case 5:
+			System.out.println("Assassino");
+			break;
+		}
+	}
+
+}
